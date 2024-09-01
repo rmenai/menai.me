428b36ff2ae133deebfe7eab98df5a0750198059
Rami Menai <rami.menai@outlook.com>
2024-09-01 03:20:52 +0200

    Update setup script

--- public/scripts/setup.sh
+++ public/scripts/setup.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-echo "Seting up dotfiles..."
+curl -L https://nixos.org/nix/install | sh
+. ~/.nix-profile/etc/profile.d/nix.sh
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+
+nix-shell "<home-manager>" -A install
+
+nix-shell -p yadm --run '
+  echo "Cloning your dotfiles repository with yadm..."
+  cd "$HOME"
+  yadm clone https://github.com/rmenai/dotfiles.git
+  yadm submodule update --recursive --init
+
+  if [ $? -ne 0 ]; then
+      echo "Failed to clone the repository. Exiting..."
+      exit 1
+  fi
+
+  echo "Dotfiles repository cloned successfully."
+'
+
+home-manager switch
+
+echo "Setup completed successfully!"
