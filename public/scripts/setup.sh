8ec29da3cfaf1f25893e87f8f0af12110a5f52a7
Rami Menai <rami.menai@outlook.com>
2024-09-01 11:55:54 +0200

    Change setup script

--- public/scripts/setup.sh
+++ public/scripts/setup.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
-curl -L https://nixos.org/nix/install | sh
-. ~/.nix-profile/etc/profile.d/nix.sh
+# Variables
+TEMP_DIR=$(mktemp -d)
+YADM_URL="https://github.com/TheLocehiliosan/yadm/raw/master/yadm"
+YADM_BIN="$TEMP_DIR/yadm"
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
+# Download yadm without sudo, to a temporary location
+echo "Downloading yadm to temporary location..."
+curl -fLo "$YADM_BIN" "$YADM_URL" && chmod a+x "$YADM_BIN"
 
-nix-shell -p yadm --run '
-  echo "Cloning your dotfiles repository with yadm..."
-  cd "$HOME"
-  yadm clone https://github.com/rmenai/dotfiles.git
-  yadm submodule update --recursive --init
+if [ $? -ne 0 ]; then
+    echo "Failed to download yadm. Exiting..."
+    exit 1
+fi
 
-  if [ $? -ne 0 ]; then
-      echo "Failed to clone the repository. Exiting..."
-      exit 1
-  fi
+echo "yadm downloaded successfully."
+cd "$HOME"
 
-  echo "Dotfiles repository cloned successfully."
-'
+# Clone your dotfiles repository and initialize submodules
+echo "Cloning your dotfiles repository with yadm..."
+"$YADM_BIN" clone https://github.com/rmenai/dotfiles.git
+"$YADM_BIN" submodule update --recursive --init
 
-nix-shell "<home-manager>" -A install
-home-manager switch
+if [ $? -ne 0 ]; then
+    echo "Failed to clone the repository. Exiting..."
+    exit 1
+fi
+
+echo "Dotfiles repository cloned successfully."
+
+# Clean up temporary yadm binary
+echo "Cleaning up..."
+rm -rf "$TEMP_DIR"
 
 echo "Setup completed successfully!"
