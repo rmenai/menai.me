d15b3c2bae6aded0bd8399ab9844b717cf0b6e3d
Rami Menai <rami.menai@outlook.com>
2024-09-01 17:43:23 +0200

    Switch to home-manager

--- public/scripts/setup.sh
+++ public/scripts/setup.sh
@@ -1,59 +1,34 @@
 #!/bin/bash
 
-# Variables
-YADM_URL="https://github.com/TheLocehiliosan/yadm/raw/master/yadm"
 GIT_URL="https://github.com/rmenai/dotfiles"
 
-# Dependencies
-if ! command -v apt &> /dev/null; then
-  echo "apt package manager could not be found. This script only supports apt-based systems."
-  exit 1
-fi
-
+# Make sure sudo is available
 if ! command -v sudo &> /dev/null; then
-  echo "sudo could not be found. Installing sudo."
-  apt update
-  apt install -y sudo
-fi
-
-sudo apt update
-sudo apt install -y git unzip
-
-# Download yadm without sudo, to a temporary location
-echo "Installing yadm..."
-sudo curl -fLo /usr/bin/yadm "$YADM_URL" && sudo chmod a+x /usr/bin/yadm
-
-if [ $? -ne 0 ]; then
-  echo "Failed to download yadm. Exiting..."
+  echo "sudo could not be found. Install sudo and try again."
   exit 1
 fi
 
-echo "yadm downloaded successfully."
-cd "$HOME"
+# Install nix
+sudo install -d -m755 -o $(id -u) -g $(id -g) /nix
+curl -L https://nixos.org/nix/install | sh
 
-echo "Checking for .profile and .zshrc files..."
-if [ -f "$HOME/.profile" ]; then
-  echo "Removing .profile..."
-  rm "$HOME/.profile"
-fi
+. $HOME/.nix-profile/etc/profile.d/nix.sh
 
-if [ -f "$HOME/.zshrc" ]; then
-  echo "Removing .zshrc..."
-  rm "$HOME/.zshrc"
-fi
+# Install home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell "<home-manager>" -A install
 
-# Clone your dotfiles repository and initialize submodules
-echo "Cloning your dotfiles repository with yadm..."
-yadm clone --bootstrap ${GIT_URL}
+# Install yadm
+nix-env -iA nixpkgs.yadm
+
+# Move the old config to backup
+sudo cp $HOME -r "$HOME.old"
+
+# Import dotfiles
+yadm clone $GIT_URL
+yadm checkout $HOME
 yadm submodule update --recursive --init
 
-if [ $? -ne 0 ]; then
-  echo "Failed to clone the repository. Exiting..."
-  exit 1
-fi
-
-echo "Dotfiles repository cloned successfully."
-
-echo "Running bootstrap"
-chmod a+x .config/yadm/bootstrap
-yadm bootstrap
+# Configure home
+home-manager switch
