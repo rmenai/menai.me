fd67a54f81fe8b45e8aaa8242d65f2514abbd0aa
Rami Menai <rami.menai@outlook.com>
2024-09-07 19:29:40 +0200

    Change to bun

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   srcDir: "src/",
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false,
+    },
+  },
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
