b072e21561dc6584771cff64e5b733059303c0e8
Rami Menai <rami@menai.me>
2024-10-24 19:29:00 +0200

    Rename script

--- src/public/scripts/dots.sh
+++ src/public/scripts/dots.sh
@@ -2,32 +2,33 @@
 GIT_URL="https://github.com/rmenai/dotfiles"
 FZF_URL="https://github.com/junegunn/fzf/releases/download/v0.55.0/fzf-0.55.0-linux_amd64.tar.gz"
 ZOXIDE_URL="https://github.com/ajeetdsouza/zoxide/releases/download/v0.9.6/zoxide_0.9.6-1_amd64.deb"
+TREESITTER_URL="https://github.com/tree-sitter/tree-sitter/releases/download/v0.24.3/tree-sitter-linux-x64.gz"
 
 # Check if the script is being run as root
 if [[ $EUID -eq 0 ]]; then
-  echo "ERROR: This script should not be run as root." > /dev/tty
-  echo "Please create a new user in the sudoers group and run the script as that user." > /dev/tty
+  echo "ERROR: This script should not be run as root." >/dev/tty
+  echo "Please create a new user in the sudoers group and run the script as that user." >/dev/tty
   exit 1
 fi
 
 # Make sure sudo is available
-if ! command -v sudo &> /dev/tty; then
-  echo "sudo could not be found. Install sudo and try again." > /dev/tty
+if ! command -v sudo &>/dev/tty; then
+  echo "sudo could not be found. Install sudo and try again." >/dev/tty
   exit 1
 fi
 
 # Warning prompt (using /dev/tty to ensure it works in non-interactive mode)
-echo "WARNING: This script will perform the following actions:" > /dev/tty
-echo "- Override these dot files: $GIT_URL." > /dev/tty
-echo "- Change the default shell to zsh." > /dev/tty
-echo "It is strongly recommended to backup your home directory before proceeding." > /dev/tty
-echo "" > /dev/tty
-read -p "Do you want to proceed? (y/N): " -n 1 -r < /dev/tty
-echo "" > /dev/tty # Move to a new line
+echo "WARNING: This script will perform the following actions:" >/dev/tty
+echo "- Override these dot files: $GIT_URL." >/dev/tty
+echo "- Change the default shell to zsh." >/dev/tty
+echo "It is strongly recommended to backup your home directory before proceeding." >/dev/tty
+echo "" >/dev/tty
+read -p "Do you want to proceed? (y/N): " -n 1 -r </dev/tty
+echo "" >/dev/tty # Move to a new line
 
 # Default to "no" if no input provided
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-  echo "Operation cancelled." > /dev/tty
+  echo "Operation cancelled." >/dev/tty
   exit 1
 fi
 
@@ -47,6 +48,10 @@ sudo chmod +x /usr/local/bin/fzf
 wget -qO zoxide.deb $ZOXIDE_URL
 sudo dpkg -i zoxide.deb
 rm zoxide.deb
+
+curl -L $TREESITTER_URL | gzip -d >tree-sitter
+sudo chmod +x tree-sitter
+sudo mv tree-sitter /usr/local/bin/
 
 # Install WSL specific dependencies
 if grep -qi microsoft /proc/version; then
