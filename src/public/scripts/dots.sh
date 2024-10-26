2aa157667afe8e6f65bc51168de2958c12590713
Rami Menai <rami@menai.me>
2024-10-27 01:09:23 +0200

    Change

--- src/public/scripts/dots.sh
+++ src/public/scripts/dots.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 GIT_URL="https://github.com/rmenai/dotfiles"
 FZF_URL="https://github.com/junegunn/fzf/releases/download/v0.55.0/fzf-0.55.0-linux_amd64.tar.gz"
+FZF_TMUX_URL="https://raw.githubusercontent.com/junegunn/fzf/master/bin/fzf-tmux"
 ZOXIDE_URL="https://github.com/ajeetdsouza/zoxide/releases/download/v0.9.6/zoxide_0.9.6-1_amd64.deb"
 TREESITTER_URL="https://github.com/tree-sitter/tree-sitter/releases/download/v0.24.3/tree-sitter-linux-x64.gz"
+YAZI_URL="https://github.com/sxyazi/yazi/releases/download/v0.3.3/yazi-x86_64-unknown-linux-gnu.zip"
+SESH_URL="https://github.com/joshmedeski/sesh/releases/download/v2.6.0/sesh_Linux_x86_64.tar.gz"
 
 # Check if the script is being run as root
 if [[ $EUID -eq 0 ]]; then
@@ -32,6 +35,10 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
   exit 1
 fi
 
+# Create a temporary directory
+TEMP_DIR=$(mktemp -d)
+cd "$TEMP_DIR" || exit
+
 # Update and add repositories
 sudo apt update
 sudo apt install software-properties-common
@@ -39,11 +46,14 @@ sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt update
 
 # Install dependencies
-sudo apt install -y git yadm zsh neovim tmux ripgrep eza xsel
+sudo apt install -y git yadm zsh neovim tmux ripgrep eza xsel bat
 
 # Install harder dependencies
 curl -L $FZF_URL | tar -xz && sudo mv fzf /usr/local/bin/
 sudo chmod +x /usr/local/bin/fzf
+
+curl -L $FZF_TMUX_URL -o /usr/local/bin/fzf-tmux
+sudo chmod +x /usr/local/bin/fzf-tmux
 
 wget -qO zoxide.deb $ZOXIDE_URL
 sudo dpkg -i zoxide.deb
@@ -52,6 +62,23 @@ rm zoxide.deb
 curl -L $TREESITTER_URL | gzip -d >tree-sitter
 sudo chmod +x tree-sitter
 sudo mv tree-sitter /usr/local/bin/
+
+# Install yazi
+wget -qO yazi.zip $YAZI_URL
+unzip -d yazi yazi.zip
+sudo mv yazi/yazi-x86_64-unknown-linux-gnu/yazi /usr/local/bin/
+sudo chmod +x /usr/local/bin/yazi
+rm -rf yazi yazi.zip
+
+# Install Sesh
+wget -qO sesh.tar.gz $SESH_URL
+tar -xvzf sesh.tar.gz
+sudo mv sesh /usr/local/bin/
+sudo chmod +x /usr/local/bin/sesh
+rm sesh.tar.gz
+
+sudo ln -s /usr/bin/batcat /usr/local/bin/bat
+bat cache --build
 
 # Install WSL specific dependencies
 if grep -qi microsoft /proc/version; then
@@ -66,3 +93,7 @@ yadm submodule update --recursive --init
 # Default shell
 sudo chsh -s $(which zsh) $USER
 zsh
+
+# Clean up temporary directory
+cd ..
+rm -rf "$TEMP_DIR"
